59d8758e8c0ea49321884a5be095b1d2327a97c1
Pedro-J-Kukul <pedro.j.kukul@gmail.com>
2025-10-24 03:07:16 -0600

    fixup: Added postgresql error handling

--- internal/data/errors.go
+++ internal/data/errors.go
@@ -1,7 +1,12 @@
 // FileName: internal/data/errors.go
 package data
 
-import "errors"
+import (
+	"errors"
+	"strings"
+
+	"github.com/lib/pq"
+)
 
 // Predefined errors for common scenarios
 var (
@@ -13,3 +18,20 @@ var (
 	ErrNoMatch             = errors.New("no matching records found")
 	ErrForeignKeyViolation = errors.New("constraint violation")
 )
+
+func isDuplicateKeyViolation(err error) bool {
+	var pqErr *pq.Error
+	if errors.As(err, &pqErr) {
+		return pqErr.Code == "23505"
+	}
+	return strings.Contains(strings.ToLower(err.Error()), "duplicate key value")
+}
+
+func isForeignKeyViolation(err error) bool {
+	var pqErr *pq.Error
+	if errors.As(err, &pqErr) {
+		return pqErr.Code == "23503"
+	}
+	lower := strings.ToLower(err.Error())
+	return strings.Contains(lower, "violates foreign key constraint")
+}
